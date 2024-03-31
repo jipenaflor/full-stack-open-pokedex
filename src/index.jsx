36795b736bcde2c3915af88fdbdfe8e0a8846560
jipenaflor <rome.img23@gmail.com>
2024-03-31 23:01:07 +0800

    Fixed linting errors

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+if (typeof window !== 'undefined') {
+    ReactDOM.render(<App />, document.getElementById('app'))
+}
+
+
